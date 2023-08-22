28e1203f1fe1113de03fc87da0f7ba5bf70a6a81
binzz <cherish.sub2nee@gamil.com>
2023-08-22 17:41:53 +0900

    [feat] : 음악재생구현
    - 각 재생목록에 맞도록 제목, 아티스트명 출력
    - play, pause 구현

---
+++ js/music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+        name: 'Honestly (feat. Ashley Alisha)',
+        artist: 'Henry Young',
+        img: 'music-1',
+        src: 'music-1',
+    },
+    {
+        name: 'One More Last Time (feat. Ashley Alisha)',
+        artist: 'Henry Young',
+        img: 'music-2',
+        src: 'music-2',
+    },
+    {
+        name: 'Play No Games (feat. Ashley Alisha)',
+        artist: 'Henry Young',
+        img: 'music-3',
+        src: 'music-3',
+    },
+    {
+        name: 'Good Love',
+        artist: 'dazeychain',
+        img: 'music-4',
+        src: 'music-4',
+    },
+    {
+        name: 'Funny Thing About Love (feat.Bel-Ami)',
+        artist: 'Aves',
+        img: 'music-5',
+        src: 'music-5',
+    },
+    {
+        name: 'Ive Been Thinkin (feat.Lulabi)',
+        artist: 'Dimitrix',
+        img: 'music-6',
+        src: 'music-6',
+    },
+    {
+        name: 'Like That',
+        artist: 'Jane & The Boy',
+        img: 'music-7',
+        src: 'music-7',
+    },
+];
---
+++ js/script.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector('.wrapper'),
+    musicImg = wrapper.querySelector('.img-area img'),
+    musicName = wrapper.querySelector('.song-details .name'),
+    musicArtist = wrapper.querySelector('.song-details .artist'),
+    mainAudio = wrapper.querySelector('#main-audio'),
+    playPauseBtn = wrapper.querySelector('.play-pause');
+
+let musicIndex = 3;
+
+window.addEventListener('load', () => {
+    loadMusic(musicIndex);
+});
+
+function loadMusic(indexNumb) {
+    musicName.innerText = allMusic[indexNumb - 1].name;
+    musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+    mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+}
+
+function playMusic() {
+    wrapper.classList.add('paused');
+    playPauseBtn.querySelector('i').innerText = 'pause';
+    mainAudio.play();
+}
+function pauseMusic() {
+    wrapper.classList.remove('paused');
+    playPauseBtn.querySelector('i').innerText = 'play_arrow';
+    mainAudio.pause();
+}
+playPauseBtn.addEventListener('click', () => {
+    const isMusicPaused = wrapper.classList.contains('paused');
+    isMusicPaused ? pauseMusic() : playMusic();
+});
